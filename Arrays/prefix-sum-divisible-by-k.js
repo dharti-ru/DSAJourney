442b2f58f17970a2f9da93fe0fcc6a070ff92f52
Dharti Upadhyay <dhartii.gohil@gmail.com>
2025-05-22 18:48:37 -0400

    feat: Added solution to find count of subarrays with sum divisible by k

--- Arrays/prefix-sum-divisible-by-k.js
+++ Arrays/prefix-sum-divisible-by-k.js
@@ -33,4 +33,36 @@ function subarrayDivisibleByK(arr,k){
     }
     return result;
 }
-console.log("subarrayDivisibleByK: ",subarrayDivisibleByK(nums,k));
+// console.log("subarrayDivisibleByK: ",subarrayDivisibleByK(nums,k));
+
+
+// Problem Statement:
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum is divisible by k.
+// Input:
+// nums = [4, 5, 0, -2, -3, 1], k = 5
+
+// Output:
+// 7
+// Explanation:
+// There are 7 subarrays with sum divisible by 5:
+// [4, 5, 0, -2, -3, 1],[5, 0, -2, -3], [5], [5, 0], [0], [0, -2, -3], [-2, -3]
+// const nums1 = [4, 5, 0, -2, -3, 1];
+const nums1 = [4, 5];
+const k1 = 5;
+function countSubarrayDivisibleByK(arr,k){
+    let count = 0;
+    let currentSum = 0;
+    let prefixMap = new Map();
+    prefixMap.set(0,0);
+    for(let i = 0; i < arr.length; i++){
+        currentSum += arr[i];
+        let mod = currentSum % k;
+        if(mod < 0 ) mod = (mod + k) % k;
+        if(prefixMap.has(mod)){
+            count += prefixMap.get(mod);
+        }
+        prefixMap.set(mod, (prefixMap.get(mod) || 0) + 1);
+    }
+    return count;
+}
+console.log("subarrayDivisibleByK: ",countSubarrayDivisibleByK(nums1,k1));
