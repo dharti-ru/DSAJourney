0ce006575eed0d8edfcd6aa29fbdaad2459a749f
Dharti Upadhyay <dhartii.gohil@gmail.com>
2025-05-22 18:51:10 -0400

    Syncing new files

---
+++ Arrays/prefix-sum-contiguous-array.js
@@ -0,0 +1,15 @@
+/* Contiguous Array
+Problem: Find the maximum length of a contiguous subarray with an equal number of 0 and 1.
+Trick: Convert 0s to -1s, then use prefix sum.
+
+Input:
+nums = [0, 1, 0, 1, 1, 0, 0]
+
+Output:
+6
+(Explanation: The subarray [0, 1, 0, 1, 1, 0] has equal number of 0s and 1s.) */
+nums = [0, 1, 0, 1, 1, 0, 0];
+function getMaxLength() {
+
+}
+console.log(getMaxLength())
