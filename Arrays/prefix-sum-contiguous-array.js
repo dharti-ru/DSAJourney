c845e8249b2002b1434d0749c97a2065eb517a05
Dharti Upadhyay <dhartii.gohil@gmail.com>
2025-05-23 17:29:32 -0400

    feat: add solution for Contiguous Array using prefix sum and Map to track balance of 0s and 1s

--- Arrays/prefix-sum-contiguous-array.js
+++ Arrays/prefix-sum-contiguous-array.js
@@ -8,8 +8,26 @@ nums = [0, 1, 0, 1, 1, 0, 0]
 Output:
 6
 (Explanation: The subarray [0, 1, 0, 1, 1, 0] has equal number of 0s and 1s.) */
-nums = [0, 1, 0, 1, 1, 0, 0];
-function getMaxLength() {
+const nums = [0, 1, 0, 1, 1, 0, 0];
+// nums = [0, 1, 1, 0, 1, 1, 1, 0];
+// nums = [0, 1];
+function getMaxLength(nums) {
+    let maxLength = 0;
+    let prefixSum = 0;
+    const sumIndexMap = new Map();
+    sumIndexMap.set(0,-1);
 
+    for(let i = 0; i < nums.length; i++){
+        prefixSum += (nums[i] === 0 ? -1 : 1);
+
+        if(sumIndexMap.has(prefixSum)){
+            let startIndex = sumIndexMap.get(prefixSum);
+            let difference = i - startIndex;
+            maxLength = Math.max(maxLength, difference);
+        }
+        else sumIndexMap.set(prefixSum, i);
+    }
+
+    return maxLength;
 }
-console.log(getMaxLength())
+console.log(getMaxLength(nums))
